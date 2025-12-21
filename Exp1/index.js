bd9f942b9279642ad0af138824ad9b92c329fe5f
Suryansh Dixit <150290655+Blissysurya@users.noreply.github.com>
2025-12-22 02:15:56 +0530

    gsap

---
+++ Exp1/index.js
@@ -0,0 +1,15 @@
+
+gsap.from('#primary',{
+    y:-20,
+    opacity:0,
+    duration:0.7,
+    delay:0.5,
+})
+
+gsap.from('#secondary div',{
+    y:-20,
+    opacity:0,
+    duration:0.7,
+    delay:1.5,
+    stagger:0.5
+})
